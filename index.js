44b7df467b09f35efedeb9e57b0e40c3e0645ddd
Zaid <zaidal.lalikai@gmail.com>
2025-12-03 23:35:21 +0700

    feat: add index.html n index.js

---
+++ index.js
@@ -0,0 +1,19 @@
+
+//var, let sama const
+//var >> variable, const >> constanta
+//untuk sekarang simple penggunaan var dihindari
+//sekarang hanya gunakan let atau const
+// untuk sesuatu yang nilainya boleh diubah gunakanlah let
+// untuk sesuatu yang nialinya tetap/ tidak berubah gunakan const
+
+//variable naming
+//gunakan naming yang merepresent
+//kalau tidak akan membingungkan di masa depan
+
+// convention naming case JS adalah 
+
+//data type number
+var number = 1;
+
+//data type string
+var string = "test"
